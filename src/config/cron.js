9468e88004bbb0f74a2bbc04a827133da5400c53
ghost-sys-coder <juniorbeast177@gmail.com>
2025-06-09 21:15:21 +0300

    Fix Cron Job Error

--- src/config/cron.js
+++ src/config/cron.js
@@ -5,7 +5,7 @@ import https from "https";
 const SERVER_URL = process.env.RENDER_SERVER_URL;
 
 // Cron Job runs every 10 minutes
-const renderServerCronJob = new cron.CronJob("*/1 * * * *", () => {
+const renderServerCronJob = new cron.CronJob("*/10 * * * *", () => {
     https.get(SERVER_URL, (res) => {
         if (res.statusCode === 200) {
             console.log("GET Request sent successfully!");
